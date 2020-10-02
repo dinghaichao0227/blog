7e96aa90d6f897b20f99a3b160f640218f798f94
unknown <>
2020-10-02 22:07:51 +0800

    wangye顶部js

---
+++ 2020-10-02.js
@@ -0,0 +1,19 @@
+window.onload=function() {
+    search();
+};
+function search() {
+    var searchBox=document.querySelector('.body1-header-box');
+    var bannerBox=document.querySelector('.body1-banner');
+    var h=bannerBox.offsetHeight;
+    window.onscroll=function(){
+        var top=document.body.scrollTop;
+        var opacity=0;
+        if (top<h) {
+            opacity=top/h*0.85
+        }else {
+            opacity=0.85;
+        }
+        searchBox.style.background="rgba(201,21,35,"+opacity+")";
+    }
+}
+这是wangye中顶部的js代码
